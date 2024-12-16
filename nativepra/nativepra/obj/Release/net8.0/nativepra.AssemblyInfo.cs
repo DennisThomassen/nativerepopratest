ca1c7e684f28f711615727031823964c5bcecb1c
Merlijn18 <merlijnvannistelrooij@gmail.com>
2024-12-16 09:18:33 +0100

    test

--- nativepra/nativepra/obj/Release/net8.0/nativepra.AssemblyInfo.cs
+++ nativepra/nativepra/obj/Release/net8.0/nativepra.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("nativepra")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dedb3068b0f8fe888e8495c852232bbe0263395c")]
 [assembly: System.Reflection.AssemblyProductAttribute("nativepra")]
